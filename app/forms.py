337a0745989f9d4f6e38d01b42cab156f7b6dd5a
Raul Taranu <marvolo@gmail.com>
2013-11-23 17:58:17 -0500

    Made the openid field a hidden field, so that the user does not have to input it

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
+from wtforms import HiddenField, BooleanField
 from wtforms.validators import Required
 
 class LoginForm(Form):
-    openid = TextField('openid', validators = [Required()])
+    openid = HiddenField('openid', validators = [Required()], default = "https://www.google.com/accounts/o8/id")
     remember_me = BooleanField('remember_me', default = False)
