0acbad4f30f583652bda42280b1e2f02795cfce8
Raul Taranu <marvolo@gmail.com>
2013-11-23 03:56:28 -0500

    Added a basic html template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from app import app
+from flask import make_response
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return make_response(open('templates/index.html').read())
+    return make_response(open(app.root_path + '/templates/index.html').read())
