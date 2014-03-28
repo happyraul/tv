a22b3776bc8eaedf3d4ed638fb04c6bca51f32ed
Raul Taranu <marvolo@gmail.com>
2014-03-28 15:40:12 -0400

    Implemented login via Google openid, restructured project using blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,29 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import config, basedir
 
-app = Flask(__name__)
-app.config.from_object('config')
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+oid = OpenID(fs_store_path=os.path.join(basedir, 'tmp'))
 
-from app import views
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    oid.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    return app
