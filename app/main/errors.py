ecd524e39114bea100a557f616765c06b9789a0a
Raul Taranu <marvolo@gmail.com>
2014-08-10 20:49:30 +0200

    Added db rollback to error

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,10 +1,12 @@
 from flask import render_template
 from . import main
+from .. import db
 
 @main.app_errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 @main.app_errorhandler(500)
-def internal_server_error(e):    
+def internal_server_error(e):
+    db.session.rollback()
     return render_template('500.html'), 500
