af3bc6bb57543c34af2a0af890ad50b138cccf7c
Raul Taranu <marvolo@gmail.com>
2014-08-11 22:38:32 +0200

    add example configuration settings, configure script to change settings

---
+++ settings.py
@@ -0,0 +1,11 @@
+environment = {
+	'DATABASE_PASSWORD': 'tv_password',
+	'DATABASE_USER': 'tv_user',
+	'DEV_DATABASE_PASSWORD': 'tv_password',
+	'DEV_DATABASE_USER': 'tv_user',
+	'MAIL_PASSWORD': 'example_password',
+	'MAIL_USERNAME': 'user@example.com',
+	'SECRET_KEY': 'changeme',
+	'TEST_DATABASE_PASSWORD': 'tv_password',
+	'TEST_DATABASE_USER': 'tv_user',
+}
