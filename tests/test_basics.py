9cacbe61d8161f3bd2135f01c19a95fdb27e4597
Raul Taranu <marvolo@gmail.com>
2014-08-12 23:09:34 +0200

    add avatar test

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,6 +1,8 @@
 import unittest
 from flask import current_app
 from app import create_app, db
+from app.models import User
+
 
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
@@ -19,3 +21,10 @@ class BasicsTestCase(unittest.TestCase):
 
     def test_app_is_testing(self):
         self.assertTrue(current_app.config['TESTING'])
+
+    def test_avatar(self):
+        u = User(name='raul', email='marvolo@gmail.com')
+        avatar = u.avatar(128)
+        expected = ('http://www.gravatar.com/avatar/059e89cce0649d6ca4e'
+                    'b1f700752422b')
+        self.assertTrue(avatar[0:len(expected)] == expected)
