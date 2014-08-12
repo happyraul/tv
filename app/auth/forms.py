88a7b39169070da9346b91c98bc1658f51985eae
Raul Taranu <marvolo@gmail.com>
2014-08-12 23:13:22 +0200

    fix lint errors, convert line endings

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,6 +2,8 @@ from flask.ext.wtf import Form
 from wtforms import HiddenField, BooleanField
 from wtforms.validators import Required
 
+
 class LoginForm(Form):
-    openid = HiddenField('openid', validators = [Required()], default = "https://www.google.com/accounts/o8/id")
-    remember_me = BooleanField('remember_me', default = False)
+    openid = HiddenField('openid', validators=[Required()],
+                         default="https://www.google.com/accounts/o8/id")
+    remember_me = BooleanField('remember_me', default=False)
