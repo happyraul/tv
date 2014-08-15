0fbb9d353cf93b1b0e52c95491f955df63e0146c
Raul Taranu <marvolo@gmail.com>
2014-08-15 20:58:01 +0200

    add submit input to form, and render it using the form attribute in the template

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,8 @@
 from flask.ext.wtf import Form
-from wtforms import TextField
+from wtforms import TextField, SubmitField
 from wtforms.validators import Required
 
 
 class EditForm(Form):
     name = TextField('name', validators=[Required()])
+    save = SubmitField('save')
