db305ba0ffe363a22ab775464c732b01c0f1ec2c
Raul Taranu <marvolo@gmail.com>
2014-08-22 18:58:14 +0200

    update get_series to create image objects and save them to amazon s3

--- app/series/tvdb.py
+++ app/series/tvdb.py
@@ -6,10 +6,8 @@ from xml.dom import minidom
 from xml.parsers.expat import ExpatError
 from flask import current_app
 from .. import models, db
-import zipfile
-
-DATA_DIR = '/home/raul/source/tv/shows/data/'
-IMAGE_DIR = '/home/raul/source/tv/shows/static/img/'
+from zipfile import ZipFile
+from StringIO import StringIO
 
 
 class TvParser():
@@ -140,42 +138,20 @@ class TvSeries():
         return TvParser.get_field(series_dom, 'lastupdated')
 
     @staticmethod
-    def get_base_series(series_id):
+    def get_series(series_id, api_key):
 
         series = models.Series.query.get(int(series_id))
-        one_day = datetime.timedelta(days=1)
-        now = datetime.datetime.utcnow()
 
-        # Check how old our data is
-        if series is None or series.last_updated < now - one_day:
-            # Need to fetch a new series record because we don't have it in our
-            # database, or it is stale
+        if series is None:
             current_app.logger.debug("Fetching series and episodes from tvdb.")
-            #app.logger.debug(series)
-            #app.logger.debug(series.last_updated)
-            #app.logger.debug(now - one_day)
-            API_KEY = '70E022FEB8032C20'
             SEARCH_URL = 'http://www.thetvdb.com/api/%s/series/%s/all/en.zip'
-            ZIP_FILE_PATH = '%s%s/%s.zip' % (DATA_DIR, series_id, series_id)
-            XML_FILE_PATH = '%s%s/en.xml' % (DATA_DIR, series_id)
-
-            url = SEARCH_URL % (API_KEY, series_id)
+            url = SEARCH_URL % (api_key, series_id)
             r = requests.get(url)
             r.raise_for_status()
-
-            # create directory for this series data
-            if not os.path.exists(DATA_DIR + str(series_id)):
-                os.makedirs(DATA_DIR + str(series_id))
-
-            with open(ZIP_FILE_PATH, "wb") as series_zip:
-                series_zip.write(r.content)
-
-            # unzip the data file
-            with zipfile.ZipFile(ZIP_FILE_PATH, 'r') as series_zip:
-                series_zip.extractall(DATA_DIR + str(series_id))
+            series_zip = ZipFile(StringIO(r.content))
 
             try:
-                dom = minidom.parse(XML_FILE_PATH)
+                dom = minidom.parse(series_zip.open('en.xml'))
             except ExpatError:
                 return {'response': False}
 
@@ -183,12 +159,6 @@ class TvSeries():
             episode_doms = [episode for episode in
                             dom.getElementsByTagName("Episode") if
                             int(TvEpisode.get_season(episode)) > 0]
-
-            # save the series banner
-            banner_url = ("http://www.thetvdb.com/banners/%s" %
-                          TvSeries.get_banner(series_dom))
-            banner_dir = (IMAGE_DIR + '%s/' % series_id) + 'series/'
-            TvBanner.save_image(banner_url, banner_dir)
 
             new_series = models.Series(
                 id=TvSeries.get_id(series_dom),
@@ -204,48 +174,49 @@ class TvSeries():
                 status=TvSeries.get_status(series_dom),
                 last_updated=datetime.datetime.utcnow())
 
-            if series is None:
-                db.session.add(new_series)
-                for episode in episode_doms:
-                    banner_url = ("http://www.thetvdb.com/banners/%s" %
-                                  TvEpisode.get_banner(episode))
-                    banner_dir = (IMAGE_DIR + '%s/%s/' %
-                                 (series_id, TvEpisode.get_id(episode)))
-                    current_app.logger.debug("Saving episode image: %s" %
-                                             banner_url)
-                    TvBanner.save_image(banner_url, banner_dir)
-                    db.session.add(models.Episode(
-                        id=TvEpisode.get_id(episode),
-                        series_id=TvSeries.get_id(series_dom),
-                        season=TvEpisode.get_season(episode),
-                        episode_number=TvEpisode.get_episode_number(episode),
-                        name=TvEpisode.get_name(episode),
-                        overview=TvEpisode.get_overview(episode),
-                        rating=TvEpisode.get_rating(episode),
-                        rating_count=TvEpisode.get_rating_count(episode),
-                        air_date=TvEpisode.get_air_date(episode)))
-            else:
-                db.session.merge(new_series)
-                for episode in episode_doms:
-                    banner_url = ("http://www.thetvdb.com/banners/%s" %
-                                  TvEpisode.get_banner(episode))
-                    banner_dir = (IMAGE_DIR + '%s/%s/' %
-                                 (series_id, TvEpisode.get_id(episode)))
-                    current_app.logger.debug("Saving episode image: %s" %
-                                             banner_url)
-                    TvBanner.save_image(banner_url, banner_dir)
-                    db.session.merge(models.Episode(
-                        id=TvEpisode.get_id(episode),
-                        series_id=TvSeries.get_id(series_dom),
-                        season=TvEpisode.get_season(episode),
-                        episode_number=TvEpisode.get_episode_number(episode),
-                        name=TvEpisode.get_name(episode),
-                        overview=TvEpisode.get_overview(episode),
-                        rating=TvEpisode.get_rating(episode),
-                        rating_count=TvEpisode.get_rating_count(episode),
-                        air_date=TvEpisode.get_air_date(episode)))
+            # save the series banner
+            banner_url = ("http://www.thetvdb.com/banners/%s" %
+                          TvSeries.get_banner(series_dom))
+            banner_dir = ('%s/series/' % series_id)
+            new_image = models.Image(
+                series_id=series_id,
+                key="%s%s" % (banner_dir, os.path.basename(banner_url)),
+                source=banner_url,
+                type='series')
 
+            db.session.add(new_series)
+            db.session.add(new_image)
+            new_image.save()
+
+            for episode in episode_doms:
+                db.session.add(models.Episode(
+                    id=TvEpisode.get_id(episode),
+                    series_id=TvSeries.get_id(series_dom),
+                    season=TvEpisode.get_season(episode),
+                    episode_number=TvEpisode.get_episode_number(episode),
+                    name=TvEpisode.get_name(episode),
+                    overview=TvEpisode.get_overview(episode),
+                    rating=TvEpisode.get_rating(episode),
+                    rating_count=TvEpisode.get_rating_count(episode),
+                    air_date=TvEpisode.get_air_date(episode)))
+
+                if (TvEpisode.get_banner(episode)):
+                    banner_url = ("http://www.thetvdb.com/banners/%s" %
+                                  TvEpisode.get_banner(episode))
+                    banner_dir = ('%s/%s/' %
+                                  (series_id, TvEpisode.get_id(episode)))
+                    episode_image = models.Image(
+                        series_id=series_id,
+                        key="%s%s" % (banner_dir,
+                                      os.path.basename(banner_url)),
+                        source=banner_url,
+                        type='series',
+                        episode_id=TvEpisode.get_id(episode))
+                    episode_image.save()
+
+                    db.session.add(episode_image)
             db.session.commit()
+
         series = models.Series.query.get(series_id)
 
         return {'series': series,
@@ -348,53 +319,3 @@ class TvBanner():
     @staticmethod
     def get_language(banner_dom):
         return TvParser.get_optional_field(banner_dom, 'Language')
-
-    @staticmethod
-    def delete_image(user_id, series_id, type, file):
-        path = (IMAGE_DIR + '%s/%s/' % ("user-" + user_id, series_id)) +\
-            type + '/' + file
-        if os.path.exists(path):
-            current_app.logger.debug("Deleting image: %s" % path)
-            os.remove(path)
-
-    @staticmethod
-    def save_image(url, dir):
-        if not os.path.exists(dir):
-            os.makedirs(dir)
-
-        path = dir + os.path.basename(url)
-
-        if not os.path.exists(path):
-            current_app.logger.debug("Saving banner: %s" % url)
-
-            r = requests.get(url)
-            if r.status_code == 200:
-                with open(path, 'wb') as f:
-                    for chunk in r:
-                        f.write(chunk)
-
-        else:
-            current_app.logger.debug("Banner: " + path + " already saved.")
-
-    @staticmethod
-    def download_images(series_id):
-        XML_FILE_PATH = DATA_DIR + '%s/banners.xml'
-
-        if not os.path.exists(XML_FILE_PATH % series_id):
-            TvSeries.get_base_series(series_id)
-
-        try:
-            dom = minidom.parse(XML_FILE_PATH % series_id)
-        except ExpatError:
-            return {'response': False}
-
-        banners = dom.getElementsByTagName("Banner")
-
-        for banner in banners:
-            if TvBanner.get_language(banner) == "en":
-                url = ("http://www.thetvdb.com/banners/%s" %
-                       TvBanner.get_banner_url(banner))
-                type = TvBanner.get_banner_type(banner)
-                dir = (IMAGE_DIR + '%s/' % series_id) + type + '/'
-
-                TvBanner.save_image(url, dir)
